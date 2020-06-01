df8c809606870406f995d998604c543808527cab
Albertus Gagas Pradana Dian Nugraha <albertusgagas@mail.ugm.ac.id>
2020-06-01 16:11:59 +0700

    update thread's contributors, post, thread

--- src/store/actions.js
+++ src/store/actions.js
@@ -9,11 +9,13 @@ export default {
     const updates = {}
     updates[`posts/${postId}`] = post
     updates[`threads/${post.threadId}/posts/${postId}`] = postId
+    updates[`threads/${post.threadId}/contributors/${post.userId}`] = post.userId
     updates[`users/${post.userId}/posts/${postId}`] = postId
     firebase.database().ref().update(updates)
       .then(() => {
         commit('setItem', {resource: 'posts', item: post, id: postId})
         commit('appendPostToThread', {parentId: post.threadId, childId: postId})
+        commit('appendContributorToThread', {parentId: post.threadId, childId: post.userId})
         commit('appendPostToUser', {parentId: post.userId, childId: postId})
         return Promise.resolve(state.posts[postId])
       })
@@ -56,12 +58,23 @@ export default {
   updateThread ({state, commit, dispatch}, {title, text, id}) {
     return new Promise((resolve, reject) => {
       const thread = state.threads[id]
-      const newThread = {...thread, title}
-      commit('setThread', {thread: newThread, threadId: id})
+      const post = state.posts[thread.firstPostId]
 
-      dispatch('updatePost', {id: thread.firstPostId, text})
+      const edited = {
+        at: Math.floor(Date.now() / 1000),
+        by: state.authId
+      }
+
+      const updates = {}
+      updates[`posts/${thread.firstPostId}/text`] = text
+      updates[`posts/${thread.firstPostId}/edited`] = edited
+      updates[`threads/${id}/title`] = title
+
+      firebase.database().ref().update(updates)
         .then(() => {
-          resolve(newThread)
+          commit('setThread', {thread: {...thread, title}, threadId: id})
+          commit('setPost', {postId: thread.firstPostId, post: {...post, text, edited}})
+          resolve(post)
         })
     })
   },
@@ -69,18 +82,17 @@ export default {
   updatePost ({state, commit}, {id, text}) {
     return new Promise((resolve, reject) => {
       const post = state.posts[id]
-      commit('setPost', {
-        postId: id,
-        post: {
-          ...post,
-          text,
-          edited: {
-            at: Math.floor(Date.now() / 1000),
-            by: state.authId
-          }
-        }
-      })
-      resolve(post)
+      const edited = {
+        at: Math.floor(Date.now() / 1000),
+        by: state.authId
+      }
+
+      const updates = {text, edited}
+      firebase.database().ref('posts').child(id).update(updates)
+        .then(() => {
+          commit('setPost', {postId: id, post: {...post, text, edited}})
+          resolve(post)
+        })
     })
   },
 
