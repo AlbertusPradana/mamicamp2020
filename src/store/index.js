f1b7570e03e93abb7e358ae71e6213cce883d8a6
Albertus Gagas Pradana Dian Nugraha <albertusgagas@mail.ugm.ac.id>
2020-06-01 12:08:22 +0700

    fetch data for all pages

--- src/store/index.js
+++ src/store/index.js
@@ -88,24 +88,44 @@ export default new Vuex.Store({
       commit('setUser', {userId: user['.key'], user})
     },
 
-    fetchThread ({dispatch}, {id}) {
-      return dispatch('fetchItem', {resource: 'threads', id, emoji: '📄'})
+    fetchCategory ({dispatch}, {id}) {
+      return dispatch('fetchItem', {resource: 'categories', id, emoji: '🏷'})
     },
 
-    fetchUser ({dispatch}, {id}) {
-      return dispatch('fetchItem', {resource: 'users', id, emoji: '🙋'})
+    fetchForum ({dispatch}, {id}) {
+      return dispatch('fetchItem', {resource: 'forums', id, emoji: '🌧'})
+    },
+
+    fetchThread ({dispatch}, {id}) {
+      return dispatch('fetchItem', {resource: 'threads', id, emoji: '📄'})
     },
 
     fetchPost ({dispatch}, {id}) {
       return dispatch('fetchItem', {resource: 'posts', id, emoji: '💬'})
     },
 
-    fetchForums ({dispatch}, {ids}) {
-      return dispatch('fetchItems', {resource: 'forums', emoji: '🌧', ids})
+    fetchUser ({dispatch}, {id}) {
+      return dispatch('fetchItem', {resource: 'users', id, emoji: '🙋'})
     },
 
-    fetchPosts ({dispatch}, {ids}) {
-      return dispatch('fetchItems', {resource: 'posts', emoji: '💬', ids})
+    fetchCategories (context, {ids}) {
+      return context.dispatch('fetchItems', {resource: 'categories', ids, emoji: '🏷'})
+    },
+
+    fetchForums (context, {ids}) {
+      return context.dispatch('fetchItems', {resource: 'forums', ids, emoji: '🌧'})
+    },
+
+    fetchThreads (context, {ids}) {
+      return context.dispatch('fetchItems', {resource: 'threads', ids, emoji: '🌧'})
+    },
+
+    fetchPosts (context, {ids}) {
+      return context.dispatch('fetchItems', {resource: 'posts', ids, emoji: '💬'})
+    },
+
+    fetchUsers (context, {ids}) {
+      return context.dispatch('fetchItems', {resource: 'users', ids, emoji: '🙋'})
     },
 
     fetchAllCategories ({state, commit}) {
@@ -121,7 +141,6 @@ export default new Vuex.Store({
         })
       })
     },
-
     fetchItem ({state, commit}, {id, emoji, resource}) {
       console.log('🔥‍', emoji, id)
       return new Promise((resolve, reject) => {
@@ -133,6 +152,7 @@ export default new Vuex.Store({
     },
 
     fetchItems ({dispatch}, {ids, resource, emoji}) {
+      ids = Array.isArray(ids) ? ids : Object.keys(ids)
       return Promise.all(ids.map(id => dispatch('fetchItem', {id, resource, emoji})))
     }
   },
@@ -146,14 +166,11 @@ export default new Vuex.Store({
     setThread (state, {thread, threadId}) {
       Vue.set(state.threads, threadId, thread)
     },
-
     setItem (state, {item, id, resource}) {
       item['.key'] = id
       Vue.set(state[resource], id, item)
     },
-
     appendPostToThread: makeAppendChildToParentMutation({parent: 'threads', child: 'posts'}),
-
     appendPostToUser: makeAppendChildToParentMutation({parent: 'users', child: 'posts'}),
     appendThreadToForum: makeAppendChildToParentMutation({parent: 'forums', child: 'threads'}),
     appendThreadToUser: makeAppendChildToParentMutation({parent: 'users', child: 'threads'})
